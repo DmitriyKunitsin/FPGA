a1ec3ebdfa167c41a8e5f411a007597b14de4eeb
Dmitriy Kunicin <d.kunucun@looch.ru>
2026-08-11 17:17:34 +0700

    add physical_constr

---
+++ Zynq_7000/fpga_02_breath_led/fpga_02_breath_led.srcs/sim_1/new/breath_led_tb.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.08.2026 16:56:18
+// Design Name: 
+// Module Name: breath_led_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+`timescale 1ns / 1ns
+module breath_led_tb ();
+
+  logic clk_reg;
+  logic rstn_reg;
+  logic [3:0] led;
+
+  logic clk;
+  logic rstn;
+
+  initial begin
+    clk_reg  = 0;
+    rstn_reg = 0;
+    #10 rstn_reg = 1;
+  end
+
+  always #1 clk_reg = ~clk_reg;
+  assign rstn = rstn_reg;
+  assign clk  = clk_reg;
+  breath_led #(
+      .CLOCK_FRQ(1000000)
+  ) breath_led_inst (
+      .clk (clk),
+      .rstn(rstn),
+      .led (led)
+  );
+endmodule
+
